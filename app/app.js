d8044b1c9b035869a9c56c192f8e18987e13b6d2
intern0001 <139584025+intern0001@users.noreply.github.com>
2023-10-02 15:01:19 +0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -39,6 +39,8 @@ app.post('/register', (req, res) => {
   res.render('success', { message: 'Registration successful!' });
 });
 
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
